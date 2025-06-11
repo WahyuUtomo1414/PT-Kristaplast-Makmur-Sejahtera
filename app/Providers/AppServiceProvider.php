24389579ca43a75db41b7a53398c38906956f03c
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-11 15:38:45 +0700

    install plugin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Filament\Support\Facades\FilamentView;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+    FilamentView::registerRenderHook(
+            'panels::auth.login.form.after',
+            fn (): string => Blade::render('@vite(\'resources/css/custom-login.css\')'),
+        );
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            content: [
+                "./vendor/diogogpinto/filament-auth-ui-enhancer/resources/**/*.blade.php",
+            ],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/custom-login.css",
+            ],
             refresh: true,
         }),
         tailwindcss(),
